b1d637c03e71e8092507fb2852aadcea4aae9ec5
ayman adel <ayman.abdelghany@aurea.com>
2017-03-27 19:14:07 +0200

    first commit

---
+++ src/main/java/com/aurea/automation/recoveryreporting/Bootstrap.java
@@ -0,0 +1,27 @@
+package com.aurea.automation.recoveryreporting;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.DockerCmdExecFactory;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.jaxrs.JerseyDockerCmdExecFactory;
+
+public class Bootstrap {
+
+	public static void main(String[] args) {
+		// System.out.println("start-----");
+		DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+				.withDockerHost("tcp://localhost:2376").withDockerTlsVerify(true)
+				.withDockerCertPath("/home/user/.docker").withRegistryUsername(registryUser)
+				.withRegistryPassword(registryPass).withRegistryEmail(registryMail).withRegistryUrl(registryUrl)
+				.build();
+		// using jaxrs/jersey implementation here (netty impl is also available)
+		DockerCmdExecFactory dockerCmdExecFactory = new JerseyDockerCmdExecFactory().withReadTimeout(1000)
+				.withConnectTimeout(1000).withMaxTotalConnections(100).withMaxPerRouteConnections(10);
+		
+		DockerClient dockerClient = DockerClientBuilder.getInstance(config)
+				.withDockerCmdExecFactory(dockerCmdExecFactory).build();
+	}
+
+}
